0b4c9017401c0b5d2f24cb4ee8def38030e3653d
Tommy <subessencial@gmail.com>
2023-05-23 10:10:39 +0100

    Add files via upload

---
+++ src/ChatStyles.js
@@ -0,0 +1,22 @@
+import { StyleSheet } from 'react-native';
+
+const styles = StyleSheet.create({
+  inputToolbar: {
+    backgroundColor: '#0E1C36', // Set the background color for the input toolbar
+  },
+  messageText: {
+    color: '#ffffff', // Set the text color for the chat messages
+  },
+  messageContainer: {
+    // Set the background color for the chat message container
+    // You can further customize the padding, border radius, etc. as per your requirements
+    backgroundColor: '#AFCBFF',
+    borderRadius: 10,
+    padding: 10,
+  },
+  bubbleText: {
+    color: 'white', // Set the text color to white
+  },
+});
+
+export default styles;
